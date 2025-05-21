d462279ecf45d75dfb0c6b5cc7cb14c9471a8e7a
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-05-21 18:22:54 -0400

    Initial commit

---
+++ src/test/java/com/jiffingwalter/personal_backend/PersonalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiffingwalter.personal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
