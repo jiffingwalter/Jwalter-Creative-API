8f5a6841991fa9c327af90dcfdc3365007211ddb
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-05-24 19:27:02 -0400

    Set up postgres database connection and validated

--- src/main/java/com/jiffingwalter/jwalter_creative_api/JwalterCreativeAPIApplication.java
+++ src/main/java/com/jiffingwalter/jwalter_creative_api/JwalterCreativeAPIApplication.java
@@ -2,9 +2,10 @@ package com.jiffingwalter.jwalter_creative_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+//(exclude = {DataSourceAutoConfiguration.class })
 public class JwalterCreativeAPIApplication {
 
 	public static void main(String[] args) {
