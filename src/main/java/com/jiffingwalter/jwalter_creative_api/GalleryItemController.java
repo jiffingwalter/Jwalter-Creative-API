8a2c56b24a8ace43a3d040c676102992df8ad148
Justin Walter <81245937+jiffingwalter@users.noreply.github.com>
2025-06-04 17:38:10 -0400

    Created dockerfile for building the container, unblocked CORS policy for local angular app (make this an environment variable!!), set postgres to not modify database on run

--- src/main/java/com/jiffingwalter/jwalter_creative_api/GalleryItemController.java
+++ src/main/java/com/jiffingwalter/jwalter_creative_api/GalleryItemController.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 import java.util.UUID;
 import java.time.LocalDateTime;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +24,7 @@ public class GalleryItemController {
     }
 
     /** API - Get all current gallery items in the GalleryItem table */
+    @CrossOrigin(origins = "http://localhost:4200/")
     @GetMapping
     public List<GalleryItem> getGalleryItems() {
         return galleryItemService.getAllGalleryItems();
@@ -37,9 +38,6 @@ public class GalleryItemController {
             return ResponseEntity.ok(result.get());
         else
             return ResponseEntity.notFound().build();
-        //return galleryItemService.getGalleryItemById(id)
-        //    .map(ResponseEntity::ok)
-        //    .orElse(ResponseEntity.notFound().build());
     }
 
     /** API - Accept a gallery item and insert a new record */
